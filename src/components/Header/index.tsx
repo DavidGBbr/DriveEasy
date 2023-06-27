65921d1be44128116e77cb3269edad89ec51992f
davidGBbr <davidgomesbrigido@gmail.com>
2023-06-27 17:51:11 -0300

    header and menu created

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import {
+  AppBar,
+  Tab,
+  Tabs,
+  Toolbar,
+  Typography,
+  useMediaQuery,
+} from "@mui/material";
+import DriveEtaIcon from "@mui/icons-material/DriveEta";
+import DrawerMenu from "../Drawer";
+import { useTheme } from "@emotion/react";
+
+const Header = () => {
+  const [value, setValue] = useState();
+  const theme = useTheme();
+  const isMatch = useMediaQuery("(min-width:600px)");
+  return (
+    <>
+      <AppBar sx={{ background: "#242731", padding: 1 }}>
+        <Toolbar>
+          <DriveEtaIcon sx={{ fontSize: "3rem" }} />
+          {isMatch ? (
+            <Tabs
+              sx={{ marginLeft: "auto" }}
+              indicatorColor="secondary"
+              textColor="inherit"
+              value={value}
+              onChange={(e, value) => setValue(value)}
+            >
+              <Tab label="Home" />
+              <Tab label="Cliente" />
+              <Tab label="Condutor" />
+            </Tabs>
+          ) : (
+            <DrawerMenu />
+          )}
+        </Toolbar>
+      </AppBar>
+    </>
+  );
+};
+
+export default Header;
